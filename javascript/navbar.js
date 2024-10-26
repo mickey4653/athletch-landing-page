03b556c12350ff790aeba4918e8beb284f497d97
Michael Ilerioluwa Adeniyi <77995341+mickey4653@users.noreply.github.com>
2024-10-25 23:12:41 -0400

    Made Some Changes2: 	- added the pricing section 	- added the footer 	- made web page responsive partially 	- made the navbar menu  mobile friendly

---
+++ javascript/navbar.js
@@ -0,0 +1,4 @@
+document.getElementById("navbarToggle").addEventListener("click", function() {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("active"); // Toggle the active class
+});

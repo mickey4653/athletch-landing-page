a3b61b5d55d3e3bc9542ef48610ad311dae20bbf
Michael Ilerioluwa Adeniyi <77995341+mickey4653@users.noreply.github.com>
2024-11-01 15:34:07 -0400

    Made Some Changes3: 	- made web page more responsive 	- adjusted placement of nav buttons 	- fixed broken youtube link

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -2,3 +2,4 @@ document.getElementById("navbarToggle").addEventListener("click", function() {
     const navLinks = document.getElementById("navLinks");
     navLinks.classList.toggle("active"); // Toggle the active class
 });
+

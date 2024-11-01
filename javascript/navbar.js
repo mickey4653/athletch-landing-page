d9aa9657c2baedf928337f1afe9b105d73c6c984
Michael Ilerioluwa Adeniyi <77995341+mickey4653@users.noreply.github.com>
2024-11-01 15:40:33 -0400

    - Removed navbar script function; wasnt needed anymore

--- javascript/navbar.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("navbarToggle").addEventListener("click", function() {
-    const navLinks = document.getElementById("navLinks");
-    navLinks.classList.toggle("active"); // Toggle the active class
-});
-
